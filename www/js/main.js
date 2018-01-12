2cbb314d4034c47d88a86f717abe1e7549e4e958
enaut <enaut.irakasle@gmail.com>
2018-01-13 00:17:23 +0100

    alkantzetatik eraikineratara pasa

--- www/js/main.js
+++ www/js/main.js
@@ -1,48 +1,68 @@
 $(document).ready(function() {
-  //console.log( "ready!" );
+	// console.log( "ready!" );
 
-  //login botoia programatu
-  $('#sartu').on("click", function(event) {
-    event.preventDefault();
-    //alert("submit sakatua");
-    login($("#loginForm").serialize());
-  });
-
-});
-
-
-$("#eraikinOrria").on("pagebeforeshow", function(event) {
-  var myObj = [{
-    "name": "Mikel",
-    "age": 33,
-    "city": "Ondarroa"
-  }, {
-    "name": "John",
-    "age": 31,
-    "city": "New York"
-  }];
-  var myJSON = JSON.stringify(myObj);
-  //alert("kargatu aurrekoa");
-  var arraia = JSON.parse(myJSON);
- for (i = 0; i < arraia.length; i++) {
-   var pertsona = arraia[i];
-    //alert(pertsona.name);
-   $('#eraikinZerrenda').append('<li><a>' + pertsona.name + ' ' + pertsona.city + '</a></li>').listview('refresh');
-} 
+	// login botoia programatu
+	$('#sartu').on("click", function(event) {
+		event.preventDefault();
+		// alert("submit sakatua");
+		login($("#loginForm").serialize());
+	});
 
 });
+
+// eraikinen orria erakutsi aurretik zerbait egiteko proba bat
+$("#eraikinOrria").on(
+		"pagebeforeshow",
+		function(event) {
+			var myObj = [ {
+				"name" : "Mikel",
+				"age" : 33,
+				"city" : "Ondarroa"
+			}, {
+				"name" : "John",
+				"age" : 31,
+				"city" : "New York"
+			} ];
+			var myJSON = JSON.stringify(myObj);
+			// alert("kargatu aurrekoa");
+			var arraia = JSON.parse(myJSON);
+			for (i = 0; i < arraia.length; i++) {
+				var pertsona = arraia[i];
+				// alert(pertsona.name);
+				$('#eraikinZerrenda').append(
+						'<li><a href="?id=' + (i + 10) + '#alkantzeak">'
+								+ pertsona.name + ' ' + pertsona.city
+								+ '</a></li>').listview('refresh');
+			}
+
+		});
+
+// alkantzeen orria kargatu aurretik eraikinaren idearekin bere datuak lortu eta
+// formularioa bete
+$('#alkantzeak').on(
+		'pagebeforeshow',
+		function(event) {
+			Console.log(decode);
+			// TODO erakinarenDatuak.php egiteke dago
+			$.post("https://enautirakasle.000webhostapp/eraikinarenDatuak.php",
+					datuak, function(data) {
+						// TODO zerbitzariaren erantzuna doa hemen
+
+					});
+		});
 
 /*
-logina egin eta zabaldu beharreko horria kargatuko du.
-*/
+ * logina egin eta zabaldu beharreko horria kargatuko du.
+ */
 function login(datuak) {
-  // alert(datuak);
-  $.post("https://enautirakasle.000webhostapp.com/login.php", datuak, function(data) {
-    if (data === "ok") {
-      $.mobile.changePage("#eraikinOrria");
-    } else {
-      $.mobile.changePage("#pageError");
-    }
-  });
+	// alert(datuak);
+	$.post("https://enautirakasle.000webhostapp.com/login.php", datuak,
+			function(data) {
+				if (data === "ok") {
+					$.mobile.changePage("#eraikinOrria");
+				} else {
+					$.mobile.changePage("#pageError");
+				}
+			});
 
 }
